f53bbe4636c21ed0ac6674ae4983137277039529
Natan <natan.couture-dumais@valtech.com>
2022-10-12 23:47:47 -0400

    Natan UI and app skeleton, done

---
+++ Assets/Scripts/AutoScroll.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AutoScroll : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        startAutoScroll();
+    }
+
+    IEnumerator AutoScrollAnim(ScrollRect srollRect, float startPosition, float endPosition, float duration)
+    {
+        yield return new WaitForSeconds(0.5f);
+        float t0 = 0.0f;
+        while (t0 < 1.0f)
+        {
+            t0 += Time.deltaTime / duration;
+            srollRect.verticalNormalizedPosition = Mathf.Lerp(startPosition, endPosition, t0);
+            yield return null;
+        }
+
+    }
+
+    public void startAutoScroll()
+    {
+        StartCoroutine(AutoScrollAnim(GameObject.Find("Scroll View").GetComponent<ScrollRect>(), 1, 0, 100f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
