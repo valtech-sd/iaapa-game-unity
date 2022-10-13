d41c1b8045dc5f5d787249240ab12d119e7ad796
Le Cabrera <le.cabrera@valtech.com>
2022-10-13 17:23:12 -0400

    Change indents from spaces to tabs

--- Assets/Scripts/AutoScroll.cs
+++ Assets/Scripts/AutoScroll.cs
@@ -5,33 +5,33 @@ using UnityEngine.UI;
 
 public class AutoScroll : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        startAutoScroll();
-    }
+	// Start is called before the first frame update
+	void Start()
+	{
+		startAutoScroll();
+	}
 
-    IEnumerator AutoScrollAnim(ScrollRect srollRect, float startPosition, float endPosition, float duration)
-    {
-        yield return new WaitForSeconds(0.5f);
-        float t0 = 0.0f;
-        while (t0 < 1.0f)
-        {
-            t0 += Time.deltaTime / duration;
-            srollRect.verticalNormalizedPosition = Mathf.Lerp(startPosition, endPosition, t0);
-            yield return null;
-        }
+	IEnumerator AutoScrollAnim(ScrollRect srollRect, float startPosition, float endPosition, float duration)
+	{
+		yield return new WaitForSeconds(0.5f);
+		float t0 = 0.0f;
+		while (t0 < 1.0f)
+		{
+			t0 += Time.deltaTime / duration;
+			srollRect.verticalNormalizedPosition = Mathf.Lerp(startPosition, endPosition, t0);
+			yield return null;
+		}
 
-    }
+	}
 
-    public void startAutoScroll()
-    {
-        StartCoroutine(AutoScrollAnim(GameObject.Find("Scroll View").GetComponent<ScrollRect>(), 1, 0, 100f));
-    }
+	public void startAutoScroll()
+	{
+		StartCoroutine(AutoScrollAnim(GameObject.Find("Scroll View").GetComponent<ScrollRect>(), 1, 0, 100f));
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
 }
--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -4,50 +4,50 @@ using UnityEngine;
 using UnityEngine.UI;
 public class Timer : MonoBehaviour
 {
-    public float totaltime = 100;
-    public float timeRemaining = 10;
-    public bool timerIsRunning = false;
-    public Text timeText;
-    private void Start()
-    {
-        // Starts the timer automatically
-        //timerIsRunning = true;
-    }
-    void Update()
-    {
-        if (timerIsRunning)
-        {
-            if (timeRemaining > 0)
-            {
-                timeRemaining -= Time.deltaTime;
-                DisplayTime(timeRemaining);
-            }
-            else
-            {
-                Debug.Log("Time has run out!");
-                timeRemaining = 0;
-                timerIsRunning = false;
-            }
-        }
-    }
-    void DisplayTime(float timeToDisplay)
-    {
-        timeToDisplay += 1;
-        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
-        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
-        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
-    }
+	public float totaltime = 100;
+	public float timeRemaining = 10;
+	public bool timerIsRunning = false;
+	public Text timeText;
+	private void Start()
+	{
+		// Starts the timer automatically
+		//timerIsRunning = true;
+	}
+	void Update()
+	{
+		if (timerIsRunning)
+		{
+			if (timeRemaining > 0)
+			{
+				timeRemaining -= Time.deltaTime;
+				DisplayTime(timeRemaining);
+			}
+			else
+			{
+				Debug.Log("Time has run out!");
+				timeRemaining = 0;
+				timerIsRunning = false;
+			}
+		}
+	}
+	void DisplayTime(float timeToDisplay)
+	{
+		timeToDisplay += 1;
+		float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+		float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+		timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+	}
 
-    public void startTimer()
-    {
-        timeRemaining = totaltime;
-        timerIsRunning = true;
-    }
+	public void startTimer()
+	{
+		timeRemaining = totaltime;
+		timerIsRunning = true;
+	}
 
-    public void resetTimerAndWait()
-    {
-        timeRemaining = totaltime;
-        DisplayTime(timeRemaining);
-        timerIsRunning = false;
-    }
+	public void resetTimerAndWait()
+	{
+		timeRemaining = totaltime;
+		DisplayTime(timeRemaining);
+		timerIsRunning = false;
+	}
 }
