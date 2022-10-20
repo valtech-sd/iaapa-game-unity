42aa35253c12d0c564e1f866fc6aa40986eebe31
Le Cabrera <le.cabrera@valtech.com>
2022-10-20 13:58:23 -0400

    IAAP-27 #time 4h Trigger flow control from rabbit mq game state message

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -4,6 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Doozy.Runtime.Nody;
+using Doozy.Runtime.UIManager.Components;
 using RabbitMQ.Client;
 using TMPro;
 using UnityEngine;
@@ -13,42 +15,91 @@ public class Game : MonoBehaviour {
 	[SerializeField] string gameStateQueueName = "qu.iaapa-unity-gamestate";
 	[SerializeField] string gameStateRoutingKey = "#.gamestate";
 
-	// We need this because we cannot easily find objects outside of main thread from the handler
-	[Header("Game Run Text Objects")]
+	// We need this because we are not allowed to find objects outside of main thread from the RabbitMQ handler
+	[Header("Flow Controller")]
+	[SerializeField] GameObject flowController;
 
+	private FlowController flowControllerComponent;
+
+	/* WE ARE USING FLOW CONTROLLER ABOVE DIRECTLY INSTEAD BECAUSE DOOZY BUTTON EVENTS CANNOT BE INVOKED.
+	THE ISSUE WITH THIS IS WE CAN'T AUTOMATICALLY CALL THE CORRECT FLOW IF THE BUTTON IS UPDATED.
+	WE ALSO HAVE TO DUPLICATE ALL EVENTS CALLED BY THE BUTTON.
+	// We need this because we are not allowed to find objects outside of main thread from the RabbitMQ handler
+	[Header("Game State Button Objects")]
+	[SerializeField] GameObject idleButton;
+	[SerializeField] GameObject loadButton;
+	[SerializeField] GameObject runButton;
+	[SerializeField] GameObject endButton;
+
+	// UI Button components from the above button objects
+	private UIButton idleButtonComponent;
+	private UIButton loadButtonComponent;
+	private UIButton runButtonComponent;
+	private UIButton endButtonComponent;
+	*/
+
+	// We need this because we are not allowed to find objects outside of main thread from the RabbitMQ handler
+	[Header("Game Run Text Objects")]
 	[SerializeField] GameObject[] runPlayerNames;
 	[SerializeField] GameObject[] runPlayerScores;
+
+	[Header("Game Run Current Turns")]
 	[SerializeField] bool[] runPlayerHasCurrentTurns;
 
-	// We need this because we cannot easily find objects outside of main thread from the handler
+	// We need this because we are not allowed to find objects outside of main thread from the RabbitMQ handler
 	[Header("Game End Text Objects")]
 	[SerializeField] GameObject[] endPlayerNames;
 	[SerializeField] GameObject[] endPlayerScores;
 
+	// List of text components from the above text objects
 	private List<TextMeshProUGUI> runPlayerNameTextComponents = new List<TextMeshProUGUI>();
 	private List<TextMeshProUGUI> runPlayerScoreTextComponents = new List<TextMeshProUGUI>();
 	private List<TextMeshProUGUI> endPlayerNameTextComponents = new List<TextMeshProUGUI>();
 	private List<TextMeshProUGUI> endPlayerScoreTextComponents = new List<TextMeshProUGUI>();
 
+	// The current text element to update
 	private TextMeshProUGUI playerNameTextElement;
 	private TextMeshProUGUI playerScoreTextElement;
 
+	// Reference to the ShowControl class instance in the game
 	private ShowControl showControl;
 
+	// The latest game state message from Rabbit MQ
 	private GameStateMessage currentGameStateData;
+
+	// Whether the text elements need updating due to a new game state message
 	private bool needsUpdate = false;
 
+	// Reference to Timer class instance in the game
+	private Timer timer;
 
 	// Start is called before the first frame update
 	void Start() {
 		showControl = FindObjectOfType<ShowControl>();
 		showControl.RegisterConsumer(gameStateQueueName, gameStateRoutingKey, HandleGameStateMessage);
 
-		// We need to find and set components here because will get "can only be run in main thread" error from handler
-		addTextComponentToList(runPlayerNames, runPlayerNameTextComponents);
-		addTextComponentToList(runPlayerScores, runPlayerScoreTextComponents);
-		addTextComponentToList(endPlayerNames, endPlayerNameTextComponents);
-		addTextComponentToList(endPlayerScores, endPlayerScoreTextComponents);
+		timer = FindObjectOfType<Timer>();
+
+		// We need to find and set components here because will get "can only be run in main thread" error from RabbitMQ handler
+
+		// Set Text Components
+		AddTextComponentToList(runPlayerNames, runPlayerNameTextComponents);
+		AddTextComponentToList(runPlayerScores, runPlayerScoreTextComponents);
+		AddTextComponentToList(endPlayerNames, endPlayerNameTextComponents);
+		AddTextComponentToList(endPlayerScores, endPlayerScoreTextComponents);
+
+		/* WE ARE USING FLOW CONTROLLER DIRECTLY INSTEAD BECAUSE DOOZY BUTTON EVENTS CANNOT BE INVOKED.
+		THE ISSUE WITH THIS IS WE CAN'T AUTOMATICALLY CALL THE CORRECT FLOW IF THE BUTTON IS UPDATED.
+		WE ALSO HAVE TO DUPLICATE ALL EVENTS CALLED BY THE BUTTON.
+		// Set Button Components
+		idleButtonComponent = idleButton.GetComponent<UIButton>();
+		loadButtonComponent = idleButton.GetComponent<UIButton>();
+		runButtonComponent = idleButton.GetComponent<UIButton>();
+		endButtonComponent = idleButton.GetComponent<UIButton>();
+		*/
+
+		// Set Flow Controller Component
+		flowControllerComponent = flowController.GetComponent<FlowController>();
 	}
 	void OnDestroy() {
 		showControl.UnRegisterConsumer(HandleGameStateMessage);
@@ -56,18 +107,18 @@ public class Game : MonoBehaviour {
 
 	void Update() {
 		if (needsUpdate) {
-			Debug.Log($"Current game state: {currentGameStateData.Data.GameStatus}");
+			TriggerFlowControl(currentGameStateData.Data.GameStatus);
 			switch (currentGameStateData.Data.GameStatus) {
 				case "idle":
 					break;
 				case "load":
-					setPlayerDataForSeats(currentGameStateData.Data);
+					SetPlayerDataForSeats(currentGameStateData.Data);
 					break;
 				case "run":
-					setPlayerDataForSeats(currentGameStateData.Data);
+					SetPlayerDataForSeats(currentGameStateData.Data);
 					break;
 				case "end":
-					setPlayerDataForSeats(currentGameStateData.Data);
+					SetPlayerDataForSeats(currentGameStateData.Data);
 					break;
 				default:
 					break;
@@ -109,14 +160,14 @@ public class Game : MonoBehaviour {
 		}
 	}
 
-	private void setPlayerDataForSeats(GameStateData gameData) {
+	private void SetPlayerDataForSeats(GameStateData gameData) {
 		foreach (var seat in gameData.Locations) {
 			Debug.Log($"Updating data for seat: {seat}");
-			setPlayerDataForSeat(gameData.GameStatus, seat);
+			SetPlayerDataForSeat(gameData.GameStatus, seat);
 		}
 	}
 
-	private void setPlayerDataForSeat(string gameState, Seat playerData) {
+	private void SetPlayerDataForSeat(string gameState, Seat playerData) {
 		// Use seat location and game state to determine which slot to populate with data
 		if (gameState == "load" || gameState == "run") {
 			playerNameTextElement = runPlayerNameTextComponents[playerData.Location - 1];
@@ -130,9 +181,52 @@ public class Game : MonoBehaviour {
 		if (!playerScoreTextElement.Equals(null)) playerScoreTextElement.text = playerData.Score.ToString();
 	}
 
-	private void addTextComponentToList(GameObject[] gameObjects, List<TextMeshProUGUI> textComponents) {
+	private void AddTextComponentToList(GameObject[] gameObjects, List<TextMeshProUGUI> textComponents) {
 		foreach (var obj in gameObjects) {
 			textComponents.Add(obj.GetComponent<TextMeshProUGUI>());
+		}
+	}
+
+	private void TriggerFlowControl(string gameState) {
+		Debug.Log($"Triggering flow control for game state: {currentGameStateData.Data.GameStatus}");
+
+		// The UIButton component uses FlowController to animate navigation to another page.
+		// We want to leverage the flow already set up on the button component by invoking it.
+		// HOWEVER, DOOZY BUTTON EVENTS DO NOT SEEM TO WORK FROM SCRIPT AT ALL :-(
+		// We are currently duplicating the events on the buttons here.
+		// If the events in the scene changes, it will also have to manually updated here!
+		switch (gameState) {
+			case "idle":
+				/*
+				Debug.Log("onClickEvent" + JsonConvert.SerializeObject(idleButtonComponent.onClickEvent));
+				Debug.Log("onSelectedEvent" + JsonConvert.SerializeObject(idleButtonComponent.onSelectedEvent));
+				//idleButtonComponent.onClickEvent.Invoke();
+				//idleButtonComponent.Click(true);
+				//idleButtonComponent.onSelectedEvent.Invoke();
+				*/
+				flowControllerComponent.SetActiveNodeByName("Idle");
+				break;
+			case "load":
+				/*
+				Debug.Log("onClickEvent" + JsonConvert.SerializeObject(loadButtonComponent.onClickEvent));
+				Debug.Log("onSelectedEvent" + JsonConvert.SerializeObject(loadButtonComponent.onSelectedEvent));
+				//loadButtonComponent.onClickEvent.Invoke();
+				//loadButtonComponent.Click(true);
+				//loadButtonComponent.onSelectedEvent.Invoke();
+				*/
+				flowControllerComponent.SetActiveNodeByName("Game");
+				break;
+			case "run":
+				//
+				//runButtonComponent.onClickEvent.Invoke();
+				flowControllerComponent.SetActiveNodeByName("Game");
+				timer.startTimer();
+				break;
+			case "end":
+				//
+				//endButtonComponent.onClickEvent.Invoke();
+				flowControllerComponent.SetActiveNodeByName("GameEnd");
+				break;
 		}
 	}
 }
--- Assets/Scripts/ShowControl.cs
+++ Assets/Scripts/ShowControl.cs
@@ -5,101 +5,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ShowControl : MonoBehaviour {
-	public enum MessageType {
-		GameState,
-		Leaderboard
-	}
-
-	[Serializable]
-	public enum PartyState {
-		Introductions,
-		Qualifiers,
-		PreTournament,
-		Tournament,
-		PostTournament,
-		FreePlays
-	}
-	[Serializable]
-	public enum GameState {
-		Idle,
-		OnBoarding,
-		Ready,
-		Running,
-		Ended
-	}
-
-	[Header("States")]
-	[SerializeField] PartyState partyState;
-	public PartyState CurrentPartyState {
-		get => partyState;
-		private set {
-			partyState = value;
-			Debug.Log($"CurrentPartyState set to {value}");
-			UpdateSecondaryView();
-		}
-	}
-	[SerializeField] GameState gameState;
-	public GameState CurrentGameState {
-		get =>  gameState;
-		private set {
-			gameState = value;
-			Debug.Log($"CurrentGameState set to {value}");
-			UpdateMainView();
-		}
-	}
-
-	/**
-	 * Sets the current party state.
-	 * NOTE: Using string because an enum argument is not supported for Unity button click handlers.
-	 * https://stackoverflow.com/questions/50131837/unity-is-not-showing-public-methods-in-the-event-field
-	 */
-	public void SetPartyState(string partyStateString) {
-		Enum.TryParse(partyStateString, out PartyState partyState);
-		CurrentPartyState = partyState;
-	}
-
-	/**
-	 * Sets the current game state.
-	 * NOTE: Using string because an enum argument is not supported for Unity button click handlers.
-	 * https://stackoverflow.com/questions/50131837/unity-is-not-showing-public-methods-in-the-event-field
-	 */
-	public void SetGameState(string gameStateString) {
-		Enum.TryParse(gameStateString, out GameState gameState);
-		CurrentGameState = gameState;
-	}
-
-	private GameObject mainCanvas;
-	private GameObject secondaryCanvas;
+	[Header("Message Bus")]
+	[SerializeField] string hostname = "localhost";
+	[SerializeField] string username = "rmqadmin";
+	[SerializeField] string password = "MTDXTnpuF0jGwDA";
+	[SerializeField] string exchangeName = "ex.iaapa-topic";
 
 	private ConnectionFactory factory;
 	private IConnection connection;
 	private EventingBasicConsumer consumer;
 
 	private IModel channel;
-	public IModel Channel {
-		get => channel;
-		private set {
-			channel = value;
-			Debug.Log($"Channel set to {value}");
-		}
-	}
-	[Header("Message Bus")]
-	[SerializeField] string hostname = "localhost";
-	[SerializeField] string username = "rmqadmin";
-	[SerializeField] string password = "MTDXTnpuF0jGwDA";
-	[SerializeField] string exchangeName = "ex.iaapa-topic";
-	public string ExchangeName {
-		get => exchangeName;
-		private set {
-			exchangeName = value;
-			Debug.Log($"MessageBusExchangeName set to {value}");
-		}
-	}
 
 	private void Awake() {
-		mainCanvas = GameObject.FindWithTag("MainCanvas");
-		secondaryCanvas = GameObject.FindWithTag("SecondaryCanvas");
-
 		// Connect to message bus
 		factory = new ConnectionFactory() { HostName = hostname, UserName = username, Password = password};
 		connection = factory.CreateConnection();
@@ -109,64 +27,19 @@ public class ShowControl : MonoBehaviour {
 		channel.ExchangeDeclare(exchangeName, "topic");
 	}
 
-	private void UpdateMainView() {
-		// NOTE: Assumes View Name contains the GameState name
-		ShowView(mainCanvas, CurrentGameState.ToString());
-	}
-	private void UpdateSecondaryView() {
-		// NOTE: Some views are shared by different PartyStates.
-		switch (CurrentPartyState) {
-			case PartyState.Qualifiers:
-				ShowView(secondaryCanvas,"HighScoresAll");
-				break;
-			case PartyState.PreTournament:
-				ShowView(secondaryCanvas,"HighScoresTop30");
-				break;
-			case PartyState.Tournament:
-				ShowView(secondaryCanvas,"Tournament");
-				break;
-			case PartyState.PostTournament:
-				ShowView(secondaryCanvas,"Tournament");
-				break;
-			case PartyState.FreePlays:
-				ShowView(secondaryCanvas,"Tournament");
-				break;
-			default:
-				ShowView(secondaryCanvas,"Idle");
-				break;
-		}
-	}
-
-	private void ShowView(GameObject canvas, string viewToShow) {
-		//Debug.Log(canvas.transform.childCount);
-
-		foreach (Transform child in canvas.transform) {
-			//Debug.Log(child.name);
-			//UIView uiView = child.GetComponent<UIView>();
-			if (child.name.Contains(viewToShow)) {
-				//uiView.Show();
-				child.gameObject.SetActive(true);
-			}
-			else {
-				//uiView.Hide();
-				child.gameObject.SetActive(false);
-			}
-		}
-	}
-
 	//public void RegisterConsumer(string messageBusQueueName, string messageBusRoutingKey, EventHandler<BasicDeliverEventArgs> handler) {
 	public void RegisterConsumer(string messageBusQueueName, string messageBusRoutingKey, BasicDeliverEventHandler
 	handler) {
 		// Make sure exchange is bound to queue
 		Debug.Log($"Binding '{messageBusQueueName}' queue to '{exchangeName}' exchange with '{messageBusRoutingKey}' routing key.");
-		Channel.QueueBind(queue: messageBusQueueName,
+		channel.QueueBind(queue: messageBusQueueName,
 			exchange: exchangeName, routingKey: messageBusRoutingKey);
 
 		Debug.Log($"Waiting for messages from '{messageBusQueueName}' queue.");
 
-		consumer = new EventingBasicConsumer(Channel);
+		consumer = new EventingBasicConsumer(channel);
 		consumer.Received += handler;
-		Channel.BasicConsume(queue: messageBusQueueName,
+		channel.BasicConsume(queue: messageBusQueueName,
 			//autoAck: true,
 			noAck: true,
 			consumer: consumer);
@@ -177,40 +50,6 @@ public class ShowControl : MonoBehaviour {
 		consumer.Received -= handler;
 	}
 
-	public GameObject FindChildWithTag(GameObject parent, string tag) {
-		GameObject child = null;
-
-		foreach(Transform transform in parent.transform) {
-			if(transform.CompareTag(tag)) {
-				child = transform.gameObject;
-				break;
-			}
-		}
-
-		return child;
-	}
-	public List<GameObject> FindChildrenWithTag(GameObject parent, string tag) {
-		List<GameObject> children = new List<GameObject>();
-
-		foreach(Transform transform in parent.transform) {
-			if(transform.CompareTag(tag)) {
-				children.Add(transform.gameObject);
-				break;
-			}
-		}
-
-		return children;
-	}
-
-	public GameObject FindChildWithName(GameObject obj, string name) {
-		Transform trans = obj.transform;
-		Transform childTrans = trans.Find(name);
-		if (childTrans != null) {
-			return childTrans.gameObject;
-		} else {
-			return null;
-		}
-	}
 
 }
 
