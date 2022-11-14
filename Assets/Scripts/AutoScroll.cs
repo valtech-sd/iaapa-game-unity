4175c6d36cf7a354720f6615b6efa994289895ff
Natan <natan.couture-dumais@valtech.com>
2022-11-14 18:05:21 -0500

    Added PingPong to the Leaderboard Autoscroll

--- Assets/Scripts/AutoScroll.cs
+++ Assets/Scripts/AutoScroll.cs
@@ -18,14 +18,40 @@ public class AutoScroll : MonoBehaviour {
 			yield return null;
 		}
 
+        StartAutoScrollReverse();
+
+
+    }
+
+    IEnumerator AutoScrollAnimReverse(ScrollRect srollRect, float startPosition, float endPosition, float duration)
+    {
+        yield return new WaitForSeconds(0.5f);
+        float t0 = 0.0f;
+        while (t0 < 1.0f)
+        {
+            t0 += Time.deltaTime / duration;
+            srollRect.verticalNormalizedPosition = Mathf.Lerp(startPosition, endPosition, t0);
+            yield return null;
+        }
+
+        StartAutoScroll();
+
+
+    }
+
+
+    public void StartAutoScroll() {
+		StartCoroutine(AutoScrollAnim(GameObject.Find("Scroll View").GetComponent<ScrollRect>(), 1, 0, 50f));
 	}
 
-	public void StartAutoScroll() {
-		StartCoroutine(AutoScrollAnim(GameObject.Find("Scroll View").GetComponent<ScrollRect>(), 1, 0, 100f));
-	}
+    public void StartAutoScrollReverse()
+    {
+        StartCoroutine(AutoScrollAnimReverse(GameObject.Find("Scroll View").GetComponent<ScrollRect>(), 0, 1, 50f));
+    }
 
-	// Update is called once per frame
-	void Update() {
+
+    // Update is called once per frame
+    void Update() {
 
 	}
 }
